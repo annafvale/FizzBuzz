68874de608aaed33b53598b92517259b699583ab
Anna Julia <79802296+annafvale@users.noreply.github.com>
2023-12-05 16:56:50 -0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,15 +1,13 @@
-function fizz_buzz(num) {
-    if (num % 15 === 0) {
-        console.log("FizzBuzz");
-    } else if (num % 5 === 0) {
-        console.log("Buzz");
-    } else if (num % 3 === 0) {
-        console.log("Fizz");
-    } else {
-        console.log(num);
-    }
-}
+#Resolution.fizzBuzz
 
-for (var i = 1; i <= 100; i++) {
-    fizz_buzz(i);
+for (let i = 1; i <= 100; i++) {
+    if (i % 15 === 0) {
+        console.log ("FizzBuzz");
+    } else if (i % 5 === 0) {
+        console.log("Fizz);
+    } else if (i % 3 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
 }
